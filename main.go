4328eb953af96f2b625412dd48e8a45dd1be1e38
preecha <preechac@hellospec.com>
2022-11-06 19:30:15 +0700

    Able to connect Database

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	DbUsername = "preechac"
+	DbPassword = "0836161840NZy"
+	DbUrl      = "postgres://" + DbUsername + ":" + DbPassword + "@127.0.0.1:5432/gle?sslmode=disable"
+)
 
 func main() {
-	router := makeRoute()
-	http.Handle("/", router)
-	http.ListenAndServe(":8000", router)
+	_, err := sql.Open("postgres", DbUrl)
+
+	if err != nil {
+		fmt.Println("databasee connect error", err)
+	}
+
+	fmt.Println("Hello, World!")
+	// router := makeRoute()
+	// http.Handle("/", router)
+	// http.ListenAndServe(":8000", router)
 }
